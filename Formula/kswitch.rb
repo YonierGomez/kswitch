ad24a5aac1f2fccc79b6ae4c90e2f370de8e56c4
Yonier <yasprill@bancolombia.com.co>
2026-02-28 01:19:43 -0500

    chore: sync Formula/kswitch.rb for v1.2.2

--- Formula/kswitch.rb
+++ Formula/kswitch.rb
@@ -1,8 +1,8 @@
 class Kswitch < Formula
   desc "Interactive Kubernetes context switcher with arrow-key navigation"
   homepage "https://github.com/YonierGomez/kswitch"
-  url "https://github.com/YonierGomez/kswitch/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "1001a75f2b5b266e5157c9710863973141d31d59ee19868f65af162d130714f3"
+  url "https://github.com/YonierGomez/kswitch/archive/refs/tags/v1.2.2.tar.gz"
+  sha256 "793c44ab67c1b89c668d4b8d38d2108b7ce106941368ccb82ed90fcfb510cfcd"
   license "MIT"
 
   depends_on "go" => :build
